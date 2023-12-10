2cd208a5d18ef0fb1e948718e4b2426fc3615095
sasaki <juanpapereas@gmail.com>
2023-12-10 17:52:27 +0100

    agrega boton de youtube, elimina lineas del css

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ function busqueda(dominio) {
     // Obten el valor del input de búsqueda
     var searchTerm = document.getElementById('searchInput').value;
     var busquedaUrl;
-    switch(dominio){
+    switch (dominio) {
         case 1:
-            busquedaUrl = 'https://www.google.com/search?q='+ encodeURIComponent(searchTerm);
+            busquedaUrl = 'https://www.google.com/search?q=' + encodeURIComponent(searchTerm);
             break;
         case 2:
-            busquedaUrl = 'https://duckduckgo.com/?q='+ encodeURIComponent(searchTerm);
+            busquedaUrl = 'https://duckduckgo.com/?q=' + encodeURIComponent(searchTerm);
             break;
         case 3:
-            busquedaUrl = 'https://www.bing.com/search?q='+ encodeURIComponent(searchTerm);
+            busquedaUrl = 'https://www.bing.com/search?q=' + encodeURIComponent(searchTerm);
+            break;
+        case 4:
+            busquedaUrl = 'https://www.youtube.com/results?search_query=' + encodeURIComponent(searchTerm);
             break;
     }
     // Redirige a la búsqueda en Google
